741a762d893f0e1d0e6006b87ec1e54bca36f606
lengbinbin <bwijixing@163.com>
2015-03-12 10:11:51 +0800

    synx 文件备份
    文件备份

---
+++ AnimationTest/控件测试/ImageLoadingTest.h
@@ -0,0 +1,13 @@
+//
+//  ImageLoadingTest.h
+//  AnimationTest
+//
+//  Created by lengbinbin on 15/3/12.
+//  Copyright (c) 2015年 lengbinbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageLoadingTest : UIViewController
+
+@end
---
+++ AnimationTest/控件测试/ImageLoadingTest.m
@@ -0,0 +1,13 @@
+//
+//  ImageLoadingTest.m
+//  AnimationTest
+//
+//  Created by lengbinbin on 15/3/12.
+//  Copyright (c) 2015年 lengbinbin. All rights reserved.
+//
+
+#import "ImageLoadingTest.h"
+
+@implementation ImageLoadingTest
+
+@end
